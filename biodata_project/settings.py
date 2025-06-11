f3657d9ebac9b40c6add9e550bf37be8b96cc4fb
Shivani Mishra <mishrashivani8780@gmail.com>
2025-06-11 15:17:37 +0530

    Changes commit

--- biodata_project/settings.py
+++ biodata_project/settings.py
@@ -190,8 +190,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'mishrashivani8780@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'drmr cnuk tasy qunp')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'bhudevnetwork@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'jpwq lijx afrc yamb')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 

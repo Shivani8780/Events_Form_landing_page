510f61288425e50932fb895a3398ad51f6f777d0
Shivani Mishra <130178096+Shivani8780@users.noreply.github.com>
2025-06-14 09:39:45 +0530

    Update admin.py

--- biodata/admin.py
+++ biodata/admin.py
@@ -151,7 +151,8 @@ def download_selected_images(modeladmin, request, queryset):
                     logging.error(f"Failed to add image for candidate {obj.candidate_name}: {e}")
     zip_buffer.seek(0)
     response = HttpResponse(zip_buffer, content_type='application/zip')
-    response['Content-Disposition'] = 'attachment; filename=selected_candidate_images.zip'
+    zip_filename = f"selected_candidate_images_{datetime.now().strftime('%Y%m%d_%H%M%S')}.zip"
+    response['Content-Disposition'] = f'attachment; filename={zip_filename}'
     return response
 
 class GalleryImageAdminForm(forms.ModelForm):
